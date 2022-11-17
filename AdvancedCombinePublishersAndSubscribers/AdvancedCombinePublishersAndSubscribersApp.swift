1bf727d546c2441e53315d4b572a8e6a9e10bb98
Simran Preet Singh <simran5590@gmail.com>
2022-11-17 06:40:33 -0800

    Initial Commit

---
+++ AdvancedCombinePublishersAndSubscribers/AdvancedCombinePublishersAndSubscribersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedCombinePublishersAndSubscribersApp.swift
+//  AdvancedCombinePublishersAndSubscribers
+//
+//  Created by Simran Preet Narang on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedCombinePublishersAndSubscribersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
